7658d60e24a0b4e198ce93fb3c42e0b6be38bf41
Tamba KOMANO <124822702+tamko23@users.noreply.github.com>
2023-10-29 23:38:38 +0100

    files added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const express = require("express");const app = express();
-const PORT = 3000;
+'use strict';
 
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
 app.get('/', (req, res) => {
-    res.send("Hello World Bienvenue sur l'application Bancash" );
+    res.send('Hello World Bienvenue sur APP Bancash');
 });
 
-app.listen( PORT, () => console.log("Server is listening to port" + PORT ));
+app.listen( PORT, HOST, () =>  {
+    console.log('Running on http://${HOST}:${PORT}');
+});
